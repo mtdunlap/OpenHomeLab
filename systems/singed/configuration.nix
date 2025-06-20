7f6d46ec1cba9c6949cf44c7a5aef26335f2527a
Matt Dunlap <trinitrotoluene5@gmail.com>
2025-06-20 01:36:24 -0400

    Add podman and podman-compose

--- systems/singed/configuration.nix
+++ systems/singed/configuration.nix
@@ -82,11 +82,12 @@
   users.users.mdunlap = {
     isNormalUser = true;
     description = "Matthew Dunlap";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "networkmanager" "wheel" "podman" ];
     packages = with pkgs; [
       kdePackages.kate
       git
       vscodium
+      podman-compose
     ];
   };
 
@@ -99,6 +100,18 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
+
+  # Enable common container config files in /etc/containers
+  virtualisation.containers.enable = true;
+  virtualisation = {
+    podman = {
+      enable = true;
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
 
   # Enable flatpak
   services.flatpak.enable = true;
